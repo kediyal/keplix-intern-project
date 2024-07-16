02a01e185a01406bb83f45f5d66dd2a4fceb9dd3
amank <amankediyal@gmail.com>
2024-07-17 01:58:23 +0530

    feat(views): add car_list_view to list all cars

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
+from .models import Car
 
-# Create your views here.
+
+# Using Function-Based-Views (FBVs) for now.
+# List view for the available cars.
+def car_list_view(request):
+    context = {
+        "cars": Car.objects.all(),
+    }
+    return TemplateResponse(request, "cars/car_list.html", context)
