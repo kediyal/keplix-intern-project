4fec5eebcbbd9dedd124153f3dc89f7de6495a07
amank <amankediyal@gmail.com>
2024-07-17 05:07:30 +0530

    feat(urls): update urlconf to support deletion

--- cars/urls.py
+++ cars/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import car_list_view, car_detail_view, car_create_view, home_view
+from .views import (
+    car_list_view,
+    car_detail_view,
+    car_create_view,
+    car_delete_view,
+    home_view,
+)
 
 urlpatterns = [
-    path("", home_view, name="home"),
+    path("", car_list_view, name="home"),
     path("cars/", car_list_view, name="car-list"),
-    path("<int:pk>/", car_detail_view, name="car-detail"),
     path("cars/new/", car_create_view, name="car-create"),
+    path("cars/<int:pk>/", car_detail_view, name="car-detail"),
+    path("cars/<int:pk>/delete/", car_delete_view, name="car-delete"),
 ]
