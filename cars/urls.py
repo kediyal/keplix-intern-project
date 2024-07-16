52204a6a2b5b4fc2713c3ddcc4f3868229a9f5a3
amank <amankediyal@gmail.com>
2024-07-17 02:47:26 +0530

    feat(urls): update urlconf for list view to support redirect

--- cars/urls.py
+++ cars/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import car_list_view, car_detail_view
+from .views import car_list_view, car_detail_view, home_view
 
 urlpatterns = [
-    path("", car_list_view, name="home"),
+    path("", home_view, name="home"),
+    path("cars/", car_list_view, name="car-list"),
     path("<int:pk>/", car_detail_view, name="car-detail"),
 ]
