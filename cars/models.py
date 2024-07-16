23c80fc7c4ca05eaaaa8205dd63177787a9368f3
amank <amankediyal@gmail.com>
2024-07-16 23:18:59 +0530

    feat(app): define Car model

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=30)
+    model = models.CharField(max_length=30)
+    year = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.year} {self.make} {self.model}"
+
+    def get_absolute_url(self):
+        return reverse("car_detail", kwargs={"pk": self.pk})
