ee162efe3e6171a99d4fccf2a320c3e720653911
amank <amankediyal@gmail.com>
2024-07-17 02:32:56 +0530

    feat(urls): update urlconf to individual car page to support primary key referencing

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import car_list_view
+from .views import car_list_view, car_detail_view
 
 urlpatterns = [
     path("", car_list_view, name="home"),
+    path("<int:pk>/", car_detail_view, name="car-detail"),
 ]
