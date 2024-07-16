a0924ea1c7fe73aa0628d3d1b9d34de1cab8e3b5
amank <amankediyal@gmail.com>
2024-07-16 23:34:14 +0530

    feat(models): update models.py to support description and author with foreign key support

--- cars/models.py
+++ cars/models.py
@@ -8,6 +8,16 @@ class Car(models.Model):
     year = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    # A user can have many-to-one relationship.
+    # In simple words, a user on the website can add multiple
+    # cars.
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
+
+    description = models.TextField()
+
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
 
