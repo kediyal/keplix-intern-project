15ae52a4ceb4226a0bc3f48ef229eb006b5c83ad
amank <amankediyal@gmail.com>
2024-07-17 09:24:14 +0530

    feat(settings): add logout_redirect_url to login/

--- cars_inventory/settings.py
+++ cars_inventory/settings.py
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
