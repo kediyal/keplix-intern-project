b3febb6d923eba6e301e2cfbdd5ea9b600e635f1
amank <amankediyal@gmail.com>
2024-07-17 11:54:36 +0530

    feat(urls): add urlconf to support car_update_view

--- cars/urls.py
+++ cars/urls.py
@@ -3,6 +3,7 @@ from .views import (
     car_list_view,
     car_detail_view,
     car_create_view,
+    car_update_view,
     car_delete_view,
     home_view,
 )
@@ -12,5 +13,6 @@ urlpatterns = [
     path("cars/", car_list_view, name="car-list"),
     path("cars/new/", car_create_view, name="car-create"),
     path("cars/<int:pk>/", car_detail_view, name="car-detail"),
+    path("cars/<int:pk>/edit/", car_update_view, name="car-update"),
     path("cars/<int:pk>/delete/", car_delete_view, name="car-delete"),
 ]
